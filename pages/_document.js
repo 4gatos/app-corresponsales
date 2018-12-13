10cb65edbb77ef1b035445723411f286169dce05
Pablo Laso <plasocortabitarte@gmail.com>
2018-12-13 01:56:51 +0100

    title

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,7 @@ export default class MyDocument extends Document {
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+          <title>Corresponsales de Guerra</title>
         </Head>
         <body className="custom_class">
           <Main />
