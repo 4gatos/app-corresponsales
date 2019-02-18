c2dcbd88c3ee5a5a2b15dbd4a15bf52aec24df55
Pablo Laso <plasocortabitarte@gmail.com>
2019-02-18 01:42:31 +0100

    prod

--- config/index.js
+++ config/index.js
@@ -1,12 +1,7 @@
-// module.exports = process.env.NODE_ENV === 'production' ? {
-//   port: process.env.PORT,
-//   apiUrl: 'https://api-corresponsales.herokuapp.com',
-// } : {
-//   port: 3000,
-//   apiUrl: 'http://localhost:4200',
-// }
-// ;
-module.exports = {
+module.exports = process.env.NODE_ENV === 'production' ? {
+  port: process.env.PORT,
+  apiUrl: 'https://api-corresponsales.herokuapp.com',
+} : {
   port: 3000,
   apiUrl: 'http://localhost:4200',
 };
